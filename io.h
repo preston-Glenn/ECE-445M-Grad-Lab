9725e1a11fc614711f6786d8080d80c6a6177f3d
tejassbhagwat99 <tejassbhagwat@gmail.com>
2023-04-20 00:14:41 -0500

    LED Blinking

---
+++ io.h
@@ -0,0 +1,4 @@
+void uart_init();
+void uart_writeText(char *buffer);
+unsigned int gpio_set(unsigned int pin_number, unsigned int value);
+unsigned int gpio_pull    (unsigned int pin_number, unsigned int value);
